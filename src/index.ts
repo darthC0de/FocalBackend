9ee6696b26e927d22f0368244128bc7ecb423c46
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2021-09-17 08:48:55 -0300

    Fix cors origin error

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,11 @@ import cors from 'cors';
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    "origin": true,
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "optionsSuccessStatus": 204
+  }));
 app.use(express.json());
 
 // Old route statements
